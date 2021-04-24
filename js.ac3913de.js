2a9c3a17cd7f04e37e0266d7505f537b0590156a
Olga Bezdetko <olgabezdetko1994@gmail.com>
2021-04-25 01:24:02 +0300

    Updates

--- js.ac3913de.js
+++ js.ac3913de.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"QvaY":[function(require,module,exports) {
 "use strict";require("../sass/main.scss");
 },{"../sass/main.scss":"clu1"}]},{},["QvaY"], null)
-//# sourceMappingURL=/goit-js-hw-10-food-service/js.9a0ccfe1.js.map
+//# sourceMappingURL=/goit-js-hw-10-food-service/js.ac3913de.js.map
